bf7fcf046ad35d1bef01cf49d89d933c6bc4f7eb
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-10-20 14:10:02 -0700

    Minor correction

--- 1_queries/5_all_users_reservations.sql
+++ 1_queries/5_all_users_reservations.sql
@@ -1,7 +1,8 @@
-SELECT reservations.id, title, cost_per_night, start_date, AVG(rating) AS average_rating
+SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
 FROM reservations
-JOIN properties ON properties.id = reservations.property_id
+JOIN properties ON reservations.property_id = properties.id
 JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
 GROUP BY properties.id, reservations.id
-ORDER BY start_date
+ORDER BY reservations.start_date
 LIMIT 10;
